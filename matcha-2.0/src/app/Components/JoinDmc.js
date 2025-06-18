e0b81e140094a3c78881d1905197e136dad2bc4a
Aaron Short <aaron.short@MacBookPro.attlocal.net>
2025-06-18 00:49:46 -0500

    about to add footer

--- matcha-2.0/src/app/Components/JoinDmc.js
+++ matcha-2.0/src/app/Components/JoinDmc.js
@@ -5,26 +5,26 @@ export default function JoinDmc() {
 
     return (
         <div className="bg-[rgb(227,243,249)]">
-        <div className="why-join flex flex-col w-[55%] mx-auto pt-[10rem]">
-          <div className="text-center text-[3rem] mb-[2rem] text-[rgb(95,129,95)]">
+        <div className="why-join flex flex-col xl:w-[55%] w-[70%] mx-auto xl:pt-[10rem] pt-[6rem]">
+          <div className="text-center xl:text-[3rem] text-3xl xl:mb-[2rem] mb-[1rem] text-[rgb(95,129,95)]">
             JOIN THE DMC!
           </div>
-          <div className="text-2xl text-center text-[rgb(95,129,95)] leading-loose">
+          <div className="text-2xl text-center text-[rgb(95,129,95)] xl:leading-loose text-xl">
           Ready to sip, chat, and make new friends? To become a member, follow @DallasMatcha on Instagram and fill out
           the membership form for newsletters.
           </div>
         </div>
-        <div className="flex mx-auto justify-center gap-[4rem] mt-[3rem] pb-[5rem]">
-            <div className="instagramBtn bg-white w-[36rem] h-[15rem] gap-[2.5rem] rounded-[2rem] text-3xl flex flex-col justify-center items-center">
+        <div className="flex mx-auto xl:flex-row flex-col justify-center items-center gap-[4rem] mt-[3rem] pb-[5rem]">
+            <div className="instagramBtn bg-white xl:w-[36rem] w-[18rem] xl:h-[15rem] h-[10rem] xl:gap-[2.5rem] gap-[1rem] rounded-[2rem] xl:text-3xl text-2xl text-center xl:text-left flex flex-col justify-center items-center">
               <div className="title text-[rgb(95,129,95)]">follow @dallasmatcha</div>
-              <Link href={"https://www.instagram.com/dallasmatcha/"} target="_blank" className="button text-white bg-[rgb(239,140,177)] w-[60%] justify-center h-[4rem] flex items-center rounded-[2rem] hover:bg-pink-400 transition cursor-pointer">follow</Link>
+              <Link href={"https://www.instagram.com/dallasmatcha/"} target="_blank" className="button text-white bg-[rgb(239,140,177)] xl:w-[60%] w-[75%] justify-center xl:h-[4rem] h-[3rem] flex items-center rounded-[2rem] hover:bg-pink-400 transition cursor-pointer">follow</Link>
             </div>
-            <div className="memberFormBtn bg-white w-[36rem] h-[15rem] gap-[2.5rem] rounded-[2rem] text-3xl flex flex-col justify-center items-center">
-              <div className="title text-[rgb(95,129,95)]">fill out membership form</div>
-              <Link href={""} target="_blank" className="button text-white bg-[rgb(239,140,177)] w-[60%] justify-center h-[4rem] flex items-center rounded-[2rem] hover:bg-pink-400 transition cursor-pointer">fill out form</Link>
+            <div className="memberFormBtn bg-white xl:w-[36rem] w-[18rem] xl:h-[15rem] h-[10rem] xl:gap-[2.5rem] gap-[1rem] rounded-[2rem] xl:text-3xl text-2xl text-center  flex flex-col justify-center items-center">
+              <div className="title xl:w-full w-[85%] text-[rgb(95,129,95)]">fill out membership form</div>
+              <Link href={""} target="_blank" className="button text-white bg-[rgb(239,140,177)] xl:w-[60%] w-[75%] justify-center xl:h-[4rem] h-[3rem] flex items-center rounded-[2rem] hover:bg-pink-400 transition cursor-pointer">fill out form</Link>
             </div>
         </div>
-        <div className="photos flex justify-center gap-[2rem] xl:pb-[4rem]">
+        <div className="photos md:flex hidden justify-center gap-[2rem] xl:pb-[4rem]">
           <div className="w-[20rem]">
              <Image 
               src={"/t-7th.png"}
--- matcha-2.0/src/app/Components/WhereNext.js
+++ matcha-2.0/src/app/Components/WhereNext.js
@@ -5,19 +5,19 @@ export default function WhereNext() {
 
     return (
       <div className="relative">
-        <div className="why-join flex flex-col w-[55%] mx-auto pt-[10rem]">
-          <div className="text-center text-[3rem] mb-[2rem] text-[rgb(95,129,95)]">
+        <div className="why-join flex flex-col xl:w-[55%] w-[70%] mx-auto pt-[6rem]">
+          <div className="text-center xl:text-[3rem] text-3xl xl:mb-[2rem] mb-[1rem] text-[rgb(95,129,95)]">
             WHERE TO NEXT?
           </div>
-          <div className="text-2xl text-center text-[rgb(95,129,95)] leading-loose">
+          <div className="xl:text-2xl text-xl text-center text-[rgb(95,129,95)] leading-loose">
           Ready to sip, chat, and make new friends? To become a member, follow @DallasMatcha on Instagram and fill out
           the membership form for newsletters. More words and stuff.
           </div>
           <div className="flex justify-center mt-[5rem] mb-[8rem] relative z-[20]">
-            <Link href={""} target="_blank" className="button text-white bg-[rgb(239,140,177)] w-[60%] justify-center h-[4rem] flex items-center rounded-[2rem] text-3xl hover:bg-pink-400 transition cursor-pointer">view cafes</Link>
+            <Link href={""} target="_blank" className="button text-white bg-[rgb(239,140,177)] xl:w-[60%] w-[75%] justify-center xl:h-[4rem] h-[3rem] flex items-center rounded-[2rem] xl:text-3xl text-2xl hover:bg-pink-400 transition cursor-pointer">view cafes</Link>
           </div>
         </div>
-        <div className="absolute bottom-0 w-[85%] left-[5rem]">
+        <div className="absolute md:block hidden bottom-0 w-[85%] xl:left-[5rem]">
           <Image 
             src={"/texas-trail.png"}
             width={10000}
